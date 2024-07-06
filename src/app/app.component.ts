3abfd4762f79a36f0c0632b9cc3bfd99398dc741
Juan Garcia <juanjoseluisgarcia@gmail.com>
2024-07-06 20:09:52 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MortgageFormComponent} from "./mortage-calculator/components/mortage-form/mortgage-form.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MortgageFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/mortage-calculator/components/mortage-form/mortgage-form.component.ts
@@ -0,0 +1,41 @@
+import {Component} from '@angular/core';
+import {HeaderComponent} from "../header/header.component";
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {MatIcon, MatIconModule} from "@angular/material/icon";
+import {MatFormField} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+
+@Component({
+  selector: 'lendwell-mortgage-form',
+  standalone: true,
+  imports: [
+    HeaderComponent,
+    ReactiveFormsModule,
+    MatIcon,
+    MatIconModule,
+    MatFormField,
+    MatInput
+  ],
+  templateUrl: './mortgage-form.component.html',
+  styleUrl: './mortgage-form.component.scss'
+})
+export class MortgageFormComponent {
+  mortgageForm!: FormGroup;
+
+
+  private setMortgageForm() {
+    this.mortgageForm = this.formBuilder.group({
+      loanAmount: ['', Validators.required, Validators.min(100000)],
+      repaymentPeriod: ['', Validators.required],
+      depositAmount: ['', [Validators.required, Validators.min(1)]],
+      propertyPrice: ['', [Validators.required, Validators.min(1)]],
+      totalIncome: ['', [Validators.required, Validators.min(1)]],
+      additionalFunding: ['']
+    });
+  }
+
+  constructor(private formBuilder: FormBuilder) {
+    this.setMortgageForm();
+  }
+
+}
